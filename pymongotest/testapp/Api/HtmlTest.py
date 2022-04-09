b57a71ed7c9b677745ef60219ba70a7ff53b1ffe
adover <adfsfsf@naver.com>
2022-04-09 13:01:00 +0900

    pymongo 테스트용

---
+++ pymongotest/testapp/Api/HtmlTest.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.template import loader
+
+from testapp.dbtesting import User
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def specific_user(request, username):
+    def get():
+        db_user_data = User().get_users_from_collection({'name': username})
+
+        user = db_user_data[0]
+        del user['_id']
+
+        template = loader.get_template('test.html')
+        return HttpResponse(template.render({'userData': [user]}, request))
+
+
+    if request.method == 'GET':
+        return get()
+    else:
+        return HttpResponse(status=405)
+
+
+def all_users(request):
+    def get():
+        dbUserData = User().get_users_from_collection({})
+
+        users = []
+        for user in dbUserData:
+            del user['_id']
+        users.append(user)
+
+        template = loader.get_template('test.html')
+        return HttpResponse(template.render({'userData': users}, request))
+
+
+    if request.method == 'GET':
+        return get()
+    else:
+        return HttpResponse(status=405)
---
+++ pymongotest/testapp/Api/JsonTest.py
@@ -0,0 +1,64 @@
+from django.http import HttpResponse
+
+import json
+
+from bson import ObjectId
+
+from testapp.dbtesting import User
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def specific_user(request, username):
+    def get():
+        users = User().get_users_from_collection({'name': username})
+        response = users[0]
+        del response['_id']
+
+        return HttpResponse(json.dumps(response), status=200)
+
+
+    def post():
+        try:
+            age, job = request.POST['age'], request.POST['job']
+        except:
+            return HttpResponse(status=400)
+
+
+        user = {
+            'name': username,
+            'age': age,
+            'job' : job
+        }
+
+        result = User().add_user_on_collection(user)
+        return HttpResponse(status=201) if result else HttpResponse(status=500)
+
+    if request.method == 'GET':
+        return get()
+    elif request.method == 'POST':
+        return post()
+    else:
+        return HttpResponse(status=405)
+
+
+def all_users(request):
+    def get():
+        users = User().get_users_from_collection({})
+        response = []
+        print(users)
+        for user in users:
+            if '_id' in user:
+                print('true')
+                hi=user['_id']
+                print(str(hi))
+            user['_id']=str(user['_id'])
+            response.append(user)
+
+        return HttpResponse(json.dumps(response), status=200)
+
+
+    if request.method == 'GET':
+        return get()
+    else:
+        return HttpResponse(status=405)
